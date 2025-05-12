ea98923ad39a47c8deb98ba15f4d7a8d90b1d8fb
Philip Lindskog <philip_lindskog@hotmail.com>
2025-05-12 12:03:41 +0200

    Add project files.

---
+++ Presentation/Controllers/EventsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Services;
+
+namespace Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EventsController(IEventService eventService) : ControllerBase
+{
+    private readonly IEventService _eventService = eventService;
+
+    // Här gör vi sen CRUD
+}
